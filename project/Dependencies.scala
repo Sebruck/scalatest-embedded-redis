788745b06ba13eae44e908b8eb1cf3c78e63ea2f
Scala Steward <me@scala-steward.org>
2020-04-30 15:19:30 +0200

    Update akka-actor to 2.6.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,5 +4,5 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.1"
   lazy val embeddedRedis = "com.github.kstyrc" % "embedded-redis" % "0.6"
   lazy val redisScala = "com.github.etaty" %% "rediscala" % "1.9.0"
-  lazy val akka = "com.typesafe.akka" %% "akka-actor" % "2.6.4"
+  lazy val akka = "com.typesafe.akka" %% "akka-actor" % "2.6.5"
 }
