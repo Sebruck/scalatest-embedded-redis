647eac52d4e8ff4ce7055be1e3ea27a5747069e4
Scala Steward <me@scala-steward.org>
2020-08-11 05:11:08 +0200

    Update scalatest to 3.1.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.1"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.3"
   lazy val embeddedRedis = "com.github.kstyrc" % "embedded-redis" % "0.6"
   lazy val redisScala = "com.github.etaty" %% "rediscala" % "1.9.0"
   lazy val akka = "com.typesafe.akka" %% "akka-actor" % "2.6.4"
